a9bca9d289892f064660b9288e8e3efd9cc07ba7
jinwoong <baseball0020@gmail.com>
2024-05-24 10:36:30 +0900

    feat: configure `HomeSceneViewController` with diffable data source

--- TodoListUIKit/Scenes/HomeScene/HomeSceneViewController.swift
+++ TodoListUIKit/Scenes/HomeScene/HomeSceneViewController.swift
@@ -17,14 +17,23 @@ protocol HomeSceneViewControllerOutput: AnyObject {
 
 final class HomeSceneViewController: UIViewController {
     // MARK: - Components
-    /// Create components which this view controller will manage
-    /*
-    private lazy var textLabel: UILabel = {
-        let textLabel = UILabel()
-
-        return textLabel
+    private lazy var tableView: UITableView = {
+        return UITableView()
     }()
-    */
+    
+    private lazy var dataSource: UITableViewDiffableDataSource<Int, Todo> = {
+        return UITableViewDiffableDataSource<Int, Todo>(tableView: tableView) { tableView, indexPath, itemIdentifier in
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: TodoTableCell.reuseableIdentifier,
+                for: indexPath
+            ) as? TodoTableCell else {
+                return UITableViewCell()
+            }
+            cell.setup()
+            
+            return cell
+        }
+    }()
 
     // MARK: - Dependencies
     var interactor: HomeSceneInteractorInput?
@@ -33,11 +42,41 @@ final class HomeSceneViewController: UIViewController {
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+        configureTableView()
     }
 
     // MARK: - Configure Components
     private func configureUI() {
-
+        view.backgroundColor = .white
+        
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let global = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: global.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: global.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: global.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: global.bottomAnchor),
+        ])
+    }
+    
+    private func configureTableView() {
+        tableView.register(TodoTableCell.self, forCellReuseIdentifier: TodoTableCell.reuseableIdentifier)
+        tableView.dataSource = dataSource
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Todo>()
+        snapshot.appendSections([0])
+//        snapshot.appendItems([Todo(completed: false, description: "1"), Todo(completed: false, description: "1")])
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    private func makeSnapshot() {
+        
     }
 }
 
