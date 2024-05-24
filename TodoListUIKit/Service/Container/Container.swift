57877fcb01f0dacacadedc0def52492219d3c0aa
jinwoong <baseball0020@gmail.com>
2024-05-24 13:39:45 +0900

    feat: add SwiftData container

---
+++ TodoListUIKit/Service/Container/Container.swift
@@ -0,0 +1,14 @@
+//
+//  Container.swift
+//  TodoListUIKit
+//
+//  Created by jinwoong Kim on 5/24/24.
+//
+
+import Foundation
+
+protocol Container<Element> {
+    associatedtype Element
+    func readAll() async throws -> [Element]
+    func write(with element: Element) async throws
+}
---
+++ TodoListUIKit/Service/Container/SwiftDataContainer.swift
@@ -0,0 +1,31 @@
+//
+//  SwiftDataContainer.swift
+//  TodoListUIKit
+//
+//  Created by jinwoong Kim on 5/24/24.
+//
+
+import Foundation
+import SwiftData
+
+@ModelActor
+actor SwiftDataContainer {
+    private func fetch<T>(_ descriptor: FetchDescriptor<T>) throws -> [T] where T: PersistentModel {
+        try self.modelContext.fetch(descriptor)
+    }
+    
+    private func insert<T>(data: T) where T: PersistentModel {
+        let context = data.modelContext ?? modelContext
+        context.insert(data)
+    }
+}
+
+extension SwiftDataContainer: Container {
+    func readAll() async throws -> [Todo] {
+        try fetch(FetchDescriptor<Todo>())
+    }
+    
+    func write(with element: Todo) async throws {
+        insert(data: element)
+    }
+}
