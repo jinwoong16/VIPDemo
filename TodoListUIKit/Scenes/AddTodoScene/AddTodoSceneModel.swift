316f16c5895b3ee4128eb2d71faf191529bb7d5d
jinwoong <baseball0020@gmail.com>
2024-05-24 11:43:34 +0900

    feat: add `AddTodoScene` model

---
+++ TodoListUIKit/Scenes/AddTodoScene/AddTodoSceneModel.swift
@@ -0,0 +1,22 @@
+//
+//  AddTodoSceneModel.swift
+//  TodoListUIKit
+//
+//  Created by jinwoong Kim on 5/24/24.
+//
+
+import Foundation
+
+enum AddTodoSceneModel {
+    enum AddTodo {
+        struct Request {
+            let body: String
+        }
+        struct Response {
+            
+        }
+        struct ViewModel {
+            
+        }
+    }
+}
