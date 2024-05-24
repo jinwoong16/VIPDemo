5e4ab07a0c31fccf26170dd567c7e1a70c3766fc
jinwoong <baseball0020@gmail.com>
2024-05-24 10:21:21 +0900

    feat: conforms `Hashable` to `Todo` entity

--- TodoListUIKit/Model/Todo.swift
+++ TodoListUIKit/Model/Todo.swift
@@ -7,21 +7,29 @@
 
 import Foundation
 
-final class Todo {
+final class Todo: Hashable {
     var id: UUID
-        var completed: Bool
-        var body: String
-        let created: Date
-        
-        init(
-            id: UUID = UUID(),
-            completed: Bool,
-            description: String,
-            created: Date = Date()
-        ) {
-            self.id = id
-            self.completed = completed
-            self.body = description
-            self.created = created
-        }
+    var completed: Bool
+    var body: String
+    let created: Date
+    
+    init(
+        id: UUID = UUID(),
+        completed: Bool,
+        description: String,
+        created: Date = Date()
+    ) {
+        self.id = id
+        self.completed = completed
+        self.body = description
+        self.created = created
+    }
+    
+    static func == (lhs: Todo, rhs: Todo) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
 }
