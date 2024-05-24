01dd92e76c2257fde67577e2ee93d56d96520086
jinwoong <baseball0020@gmail.com>
2024-05-24 13:08:07 +0900

    feat: add `AddTodoScene` in factory

--- TodoListUIKit/Factories/SceneFactory.swift
+++ TodoListUIKit/Factories/SceneFactory.swift
@@ -9,10 +9,15 @@ import UIKit
 
 protocol SceneFactory {
     static func makeHomeScene(with configurator: any HomeSceneConfigurator) -> UIViewController
+    static func makeAddTodoScene(with configurator: any AddTodoSceneConfigurator) -> UIViewController
 }
 
 final class DefaultSceneFactory: SceneFactory {
     static func makeHomeScene(with configurator: any HomeSceneConfigurator) -> UIViewController {
         return configurator.configure(HomeSceneViewController())
+    }
+    
+    static func makeAddTodoScene(with configurator: any AddTodoSceneConfigurator) -> UIViewController {
+        return configurator.configure(AddTodoSceneViewController())
     }
 }
